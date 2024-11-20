874df557d00bc7ca3bb2d133bb82e61f4def62b6
sabrine kassdallah <sabrinekasdallah2017@gmail.com>
2024-11-20 01:00:53 +0100

    add cucumber report plugin and screenchot to repor

--- src/test/java/com/e2eTests/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2eTests/automation/RunWebSuiteTest.java
@@ -17,8 +17,8 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
  * files, step definitions, reporting integration.
  */
 @CucumberOptions(features = { "src/spec/features" }, 
-                plugin = { "pretty","html:target/cucumber-report.html","json:target/cucumber.json"}, 
-                tags = "@P1 and @P2",
+                plugin = { "pretty","json:target/json-report/cucumber.json"},
+                tags = "@P1 or @P2",
 		        // glue = {"step_definitions"},
 		       monochrome = false, 
 		       snippets = CAMELCASE,
